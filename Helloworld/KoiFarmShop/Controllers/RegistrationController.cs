adc58785164e1de42f2baad3ec4fa1a033826568
hoangnhse170060 <hoangnhse170060@fpt.edu.vn>
2024-09-24 12:45:27 +0700

    form api

---
+++ Helloworld/KoiFarmShop/Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using KoiFarmShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace KoiFarmShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public string registration(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("ToysCon").ToString());
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(Username,Password,Email,IsActive) Value('" +registration.UserName+ "','" +registration.UserName+ "','" +registration.Password+ "','" +registration.Email+"','" +registration.IsActive+"')", con);
+            con.Open();
+            int i  = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            
+                return "Data inserted";
+            
+
+            else
+            
+                return "Error";
+            
+            
+
+        }   
+
+        
+        
+    }
+}
---
+++ Helloworld/KoiFarmShop/Models/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace KoiFarmShop.Models
+{
+    public class Registration
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int IsActive { get; set; }
+
+    }
+}
