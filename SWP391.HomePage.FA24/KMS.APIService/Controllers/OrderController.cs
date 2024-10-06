04637adcc975764cde1939599acb65aaaa8a8d82
hoangnhse170060 <hoangnhse170060@fpt.edu.vn>
2024-10-06 23:16:04 +0700

    OrderController

---
+++ SWP391.HomePage.FA24/KMS.APIService/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using KMG.Repository.Models;
+using KMG.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KMS.APIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public  OrderController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+         [HttpGet]
+         public async Task<ActionResult<IEnumerable<Order>>> GetOrder()
+            {
+                var orders = await _unitOfWork.OrderRepository.GetAllAsync();
+                return Ok(orders);
+            }
+
+        
+
+
+        //public class KoiController : ControllerBase
+        //{
+        //    private readonly UnitOfWork _unitOfWork;
+        //    public KoiController(UnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        //    [HttpGet]
+        //    public async Task<ActionResult<IEnumerable<Koi>>>
+        //        GetKoi()
+        //    {
+        //        return await _unitOfWork.KoiRepository.GetAllAsync();
+        //    }
+
+    }
+    }
