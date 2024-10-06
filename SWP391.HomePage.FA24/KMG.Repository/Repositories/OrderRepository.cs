fd7a3666faa76b5f6864c8471a9629b653ed65c5
hoangnhse170060 <hoangnhse170060@fpt.edu.vn>
2024-10-06 23:29:59 +0700

    OrderRepo

---
+++ SWP391.HomePage.FA24/KMG.Repository/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using KMG.Repository.Base;
+using KMG.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMG.Repository.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>
+    {
+        public OrderRepository(SwpkoiFarmShopContext context) => _context = context;
+    }
+}
