3e8798f50b9509dd88d4d1199531fdff2516dc39
Nguyễn Hữu Hoàng <Hoangnhse170060@fpt.edu.vn>
2024-10-23 08:55:22 +0700

    add

--- SWP391.HomePage.FA24/KMS.APIService/obj/Debug/net8.0/KMS.APIService.AssemblyInfo.cs
+++ SWP391.HomePage.FA24/KMS.APIService/obj/Debug/net8.0/KMS.APIService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KMS.APIService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d116b0f114f27e5874447933c9dccd369574a85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed5559b985c4678ed91836e97e04029e47261f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KMS.APIService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KMS.APIService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
